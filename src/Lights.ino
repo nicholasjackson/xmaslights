d5ba8991562f162c1ded5073f60e895992a4db89
Nic Jackson <jackson.nic@gmail.com>
2016-12-27 18:06:39 +0000

    Updated to add environment variables

---
+++ src/Lights.ino
@@ -0,0 +1,230 @@
+/*
+  Example for receiving
+  
+  https://github.com/sui77/rc-switch/
+  
+  If you want to visualize a telegram copy the raw data and 
+  paste it into http://test.sui.li/oszi/
+*/
+#include <Adafruit_CC3000.h>
+#include <SPI.h>
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+// These are the pins for the CC3000 chip if you are using a breakout board
+#define ADAFRUIT_CC3000_IRQ   3
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+
+// Create CC3000 instance
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
+                                         SPI_CLOCK_DIV2);
+// Your WiFi SSID and password these can also be set using platform.io environment variables
+//#define WLAN_SSID       "SSID"
+//#define WLAN_PASS       "password"
+#define WLAN_SECURITY   WLAN_SEC_WPA2
+
+// The port to listen for incoming TCP connections
+#define LISTEN_PORT           8099
+
+#define MAX_ACTION            10      // Maximum length of the HTTP action that can be parsed.
+
+#define MAX_PATH              64      // Maximum length of the HTTP request path that can be parsed.
+                                      // There isn't much memory available so keep this short!
+
+#define BUFFER_SIZE           MAX_ACTION + MAX_PATH + 20  // Size of buffer for incoming request data.
+                                                          // Since only the first line is parsed this
+                                                          // needs to be as large as the maximum action
+                                                          // and path plus a little for whitespace and
+                                                          // HTTP version.
+
+#define TIMEOUT_MS            500    // Amount of time in milliseconds to wait for
+                                     // an incoming request to finish.  Don't set this
+                                     // too high or your server could be slow to respond.
+// Server instance
+Adafruit_CC3000_Server httpServer(LISTEN_PORT);
+uint8_t buffer[BUFFER_SIZE+1];
+int bufindex = 0;
+char action[MAX_ACTION+1];
+char path[MAX_PATH+1];
+
+void setup() {
+  Serial.begin(9600);
+
+  setupWifi();
+  setupRF();
+  
+  // Start listening for connections
+  httpServer.begin();
+
+  Serial.println("Started");
+}
+
+String status = "";
+
+void loop() {
+  // Handle REST calls
+  handleRequest();
+
+  if (!cc3000.checkConnected()) {
+    setupWifi(); // if the wifi has lost connection reconnect
+  }
+}
+
+void setupWifi(void) {
+  Serial.println("Configuring WIFI");
+
+  if (!cc3000.begin())
+  {
+    while(1);
+  }
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    while(1);
+  }
+  while (!cc3000.checkDHCP())
+  {
+    delay(100);
+  }
+
+  // Print CC3000 IP address. Enable if mDNS doesn't work
+  while (!displayConnectionDetails()) {
+    delay(1000);
+  }
+
+}
+
+void setupRF(void) {
+  Serial.println("Configuring RF");
+
+  mySwitch.enableTransmit(10);
+}
+
+void handleRequest(void) {
+  Adafruit_CC3000_ClientRef client = httpServer.available();
+  if (client) {
+    Serial.println(F("Client connected."));
+    
+    // Clear the incoming data buffer and point to the beginning of it.
+    bufindex = 0;
+    memset(&buffer, 0, sizeof(buffer));
+    
+    // Clear action and path strings.
+    memset(&action, 0, sizeof(action));
+    memset(&path,   0, sizeof(path));
+
+    // Set a timeout for reading all the incoming data.
+    unsigned long endtime = millis() + TIMEOUT_MS;
+    
+    // Read all the incoming data until it can be parsed or the timeout expires.
+    bool parsed = false;
+    while (!parsed && (millis() < endtime) && (bufindex < BUFFER_SIZE)) {
+      if (client.available()) {
+        buffer[bufindex++] = client.read();
+      }
+      parsed = parseRequest(buffer, bufindex, action, path);
+    }
+
+    // Handle the request if it was parsed.
+    if (parsed) {
+      Serial.println(F("Processing request"));
+      Serial.print(F("Action: ")); Serial.println(action);
+      Serial.print(F("Path: ")); Serial.println(path);
+      // Check the action to see if it was a GET request.
+      if (strcmp(action, "GET") == 0) {
+        if (strcmp(path, "/on") == 0) {
+          switchOn();
+        }
+
+        if (strcmp(path, "/off") == 0) {
+          switchOff();
+        }
+
+        // Respond with the path that was accessed.
+        // First send the success response code.
+        client.fastrprintln(F("HTTP/1.1 200 OK"));
+        // Then send a few headers to identify the type of data returned and that
+        // the connection will not be held open.
+        client.fastrprintln(F("Content-Type: text/plain"));
+        client.fastrprintln(F("Status: 200 OK"));
+        
+        client.fastrprintln(F(""));
+        // Now send the response data.
+        client.fastrprintln(F("Hello world!"));
+      }
+    }
+
+    delay(100);
+
+    // Close the connection when done.
+    Serial.println(F("Client disconnected"));
+    client.close();
+  }
+}
+
+// Return true if the buffer contains an HTTP request.  Also returns the request
+// path and action strings if the request was parsed.  This does not attempt to
+// parse any HTTP headers because there really isn't enough memory to process
+// them all.
+// HTTP request looks like:
+//  [method] [path] [version] \r\n
+//  Header_key_1: Header_value_1 \r\n
+//  ...
+//  Header_key_n: Header_value_n \r\n
+//  \r\n
+bool parseRequest(uint8_t* buf, int bufSize, char* action, char* path) {
+  // Check if the request ends with \r\n to signal end of first line.
+  if (bufSize < 2)
+    return false;
+  if (buf[bufSize-2] == '\r' && buf[bufSize-1] == '\n') {
+    parseFirstLine((char*)buf, action, path);
+    return true;
+  }
+  return false;
+}
+
+// Parse the action and path from the first line of an HTTP request.
+void parseFirstLine(char* line, char* action, char* path) {
+  // Parse first word up to whitespace as action.
+  char* lineaction = strtok(line, " ");
+  if (lineaction != NULL)
+    strncpy(action, lineaction, MAX_ACTION);
+  // Parse second word up to whitespace as path.
+  char* linepath = strtok(NULL, " ");
+  if (linepath != NULL)
+    strncpy(path, linepath, MAX_PATH);
+}
+
+// Print connection details of the CC3000 chip
+bool displayConnectionDetails(void)
+{
+  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+
+  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+  {
+    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+    return false;
+  }
+  else
+  {
+    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
+    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
+    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+    Serial.println();
+    return true;
+  }
+}
+
+void switchOff(void) {
+  Serial.println("Off");
+  mySwitch.send("000000000000000000010100");
+  mySwitch.send("000000000100000000010100");
+}
+
+void switchOn(void) {
+  Serial.println("On");
+  mySwitch.send("000000000000000000010101");
+  mySwitch.send("000000000100000000010101");
+}
